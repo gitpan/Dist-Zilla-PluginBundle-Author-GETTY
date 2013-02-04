a43808dd5afff8544f7c40468870b12d6bb4b27e
Torsten Raudssus <torsten@raudssus.de>
2013-02-04 02:11:02 +0000

    Import of GETTY/Dist-Zilla-PluginBundle-Author-GETTY-0.005 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-Author-GETTY
    gitpan-cpan-version:      0.005
    gitpan-cpan-path:         GETTY/Dist-Zilla-PluginBundle-Author-GETTY-0.005.tar.gz
    gitpan-cpan-author:       GETTY
    gitpan-cpan-maturity:     released

--- lib/Pod/Weaver/PluginBundle/Author/GETTY.pm
+++ lib/Pod/Weaver/PluginBundle/Author/GETTY.pm
@@ -5,7 +5,7 @@ BEGIN {
   $Pod::Weaver::PluginBundle::Author::GETTY::AUTHORITY = 'cpan:GETTY';
 }
 {
-  $Pod::Weaver::PluginBundle::Author::GETTY::VERSION = '0.004';
+  $Pod::Weaver::PluginBundle::Author::GETTY::VERSION = '0.005';
 }
 # ABSTRACT: GETTY's default Pod::Weaver config
 
@@ -60,7 +60,7 @@ Pod::Weaver::PluginBundle::Author::GETTY - GETTY's default Pod::Weaver config
 
 =head1 VERSION
 
-version 0.004
+version 0.005
 
 =head1 DESCRIPTION
 
