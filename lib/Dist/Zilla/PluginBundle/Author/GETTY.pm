6cf07ed1e016b9aa1757282c046507e3d71fbf97
Torsten Raudssus <torsten@raudssus.de>
2014-07-07 05:45:08 +0000

    Import of GETTY/Dist-Zilla-PluginBundle-Author-GETTY-0.101 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-Author-GETTY
    gitpan-cpan-version:      0.101
    gitpan-cpan-path:         GETTY/Dist-Zilla-PluginBundle-Author-GETTY-0.101.tar.gz
    gitpan-cpan-author:       GETTY
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/PluginBundle/Author/GETTY.pm
+++ lib/Dist/Zilla/PluginBundle/Author/GETTY.pm
@@ -3,7 +3,7 @@ BEGIN {
   $Dist::Zilla::PluginBundle::Author::GETTY::AUTHORITY = 'cpan:GETTY';
 }
 # ABSTRACT: BeLike::GETTY when you build your dists
-$Dist::Zilla::PluginBundle::Author::GETTY::VERSION = '0.100';
+$Dist::Zilla::PluginBundle::Author::GETTY::VERSION = '0.101';
 use Moose;
 use Moose::Autobox;
 use Dist::Zilla;
@@ -381,7 +381,7 @@ Dist::Zilla::PluginBundle::Author::GETTY - BeLike::GETTY when you build your dis
 
 =head1 VERSION
 
-version 0.100
+version 0.101
 
 =head1 SYNOPSIS
 
